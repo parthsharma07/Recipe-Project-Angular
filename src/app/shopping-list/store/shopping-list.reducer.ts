e062060015a8341b78ae6f582e73dcf10cf72551
PARTH <parthcricketer07@gmail.com>
2020-04-10 13:16:15 +0530

    NgRx store implementation in progress

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,19 +1,58 @@
 import { Ingredient } from '../../shared/ingredient.model';
-import { Action } from '@ngrx/store';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
-const initialState = {
+export interface State{
+    ingredients: Ingredient[],
+    editedIngredient: Ingredient,
+    editedIndex: number
+}
+
+export interface AppState{
+    shoppingList: State
+}
+
+const initialState: State = {
     ingredients: [
-        new Ingredient('Apples', 5),new Ingredient('Tomatoes', 10)
-      ]
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+      ],
+    editedIngredient: null,
+    editedIndex: -1
 };
 
-export function ShoppingListReducer(state = initialState, action: Action) {
+export function ShoppingListReducer(state: State = initialState, action: ShoppingListActions.ShoppingListActions) {
     switch(action.type){
-        case ADD_INGREDIENT:
+        case ShoppingListActions.ADD_INGREDIENT:
             return {
                 ...state,
-                ingredients: [...state.ingredients, action]
+                ingredients: [...state.ingredients, action.payload]
+            };
+        case ShoppingListActions.ADD_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: [...state.ingredients, ...action.payload]
+            };
+        case ShoppingListActions.UPDATE_INGREDIENT:
+            const ingredient = state.ingredients[action.payload.index];
+            const updatedIngredient = {
+                ...ingredient,
+                ...action.payload.ingredient
             }
+            const updatedIngredients = [...state.ingredients];
+            updatedIngredients[action.payload.index] = updatedIngredient;
+
+            return {
+                ...state,
+                ingredients: updatedIngredients
+            };
+        case ShoppingListActions.DELETE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: state.ingredients.filter((ig, igIndex) => {
+                    return igIndex !== action.payload;
+                })
+            };
+        default:
+            return state;
     }
 }
