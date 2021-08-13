814c31e67a32fe2a4f33e8bbbd06d01d71468bde
parthsharma07 <parthcricketer07@gmail.com>
2021-08-13 22:59:09 +0530

    Update shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -22,7 +22,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.ingredients = this.store.select('shoppingList');
-    // this.igChangeSub = this.slService.ingredientsChanged.subscribe(
+    // this.igChangeSub = this.slService.
 
     this.loggingService.printLog('hello from shopping list component');
   }
